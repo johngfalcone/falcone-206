eee86650815285d52556d28a00e2a00384b9ef97
John Falcone <falcone@umich.edu>
2017-10-14 19:22:22 -0400

    HW6 Part B COMPLETE

--- HW6/hw6partb.py
+++ HW6/hw6partb.py
@@ -8,12 +8,40 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+testurl = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
+
+url = input('Enter link - ')
+#url = testurl
+count = int(input('Enter count - '))
+position = int(input("Enter position - "))
+
+print("Retrieving: ", url)
+
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
+linklist = []
+names = []
+#url2 = urllib.request.urlopen(names[position], context=ctx).read()
+#print(url2)
+
+for x in range(count):
+	#print(x)
+	html = urllib.request.urlopen(url, context=ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
 # Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+	tags = soup('a')
+#for tag in tags:
+    #print(tag.get('href', None))
+    #linklist.append(tag)
+
+	for tag in tags:
+		mytag = tag.get('href', None)
+		linklist.append(mytag)
 #another test for commit
+	#print(linklist)
+	newlink = linklist[position-1]
+	del linklist[:]
+	print("Retrieving: ", newlink)
+
+	url = newlink
