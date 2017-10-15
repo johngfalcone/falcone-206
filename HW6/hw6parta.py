002c1d85291f5658f931962967a2498f744ffbad
John Falcone <falcone@umich.edu>
2017-10-14 20:06:14 -0400

    HW6 Part A COMPLETE

--- HW6/hw6parta.py
+++ HW6/hw6parta.py
@@ -1,4 +1,40 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import urllib.parse
+import urllib.error
 import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+testurl = "http://py4e-data.dr-chuck.net/comments_42.html"
+
+#url = testurl
+url = input('Enter link - ')
+
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup.find_all('span')
+print(tags)
+
+numlist = []
+
+for tag in tags:
+	# Look at the parts of a tag
+	print('TAG:', tag)
+	print('URL:', tag.get('href', None))
+	print('Contents:', tag.contents[0])
+	print('Attrs:', tag.attrs)
+
+	mystring = tag.string
+	mynum = int(mystring)
+	print(mystring)
+
+	numlist.append(mynum)
+
+mysum = sum(numlist)
+print("The count is ", len(numlist))
+print("The total number is ", mysum)
 
