5333959f16b43b54e1cbbc293f737320bf182da6
John Falcone <falcone@umich.edu>
2017-12-09 23:43:04 -0500

    DB Changes

--- Final Project/database_test.py
+++ Final Project/database_test.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 
-conn = sqlite3.connect("country_weather_db.sqlite")
+conn = sqlite3.connect("state_weather_db.sqlite")
 cur = conn.cursor()
 
 cur.execute("DROP TABLE IF EXISTS WEATHER")
-cur.execute('CREATE TABLE WEATHER (city TEXT NOT NULL PRIMARY KEY UNIQUE, temp INTEGER, data DATETIME)')
+cur.execute('CREATE TABLE WEATHER (city TEXT NOT NULL PRIMARY KEY UNIQUE, temp_hi INTEGER, temp_low INTEGER, data DATETIME)')
