962c1eb49ddf5b6904ea45a703296b20174ea39d
John Falcone <falcone@umich.edu>
2017-12-13 23:20:43 -0500

    Final Project Commented

--- Final Project/final_project_main.py
+++ Final Project/final_project_main.py
@@ -7,6 +7,7 @@ import pprint
 import sqlite3
 from datetime import date, datetime
 
+#Here, the program calls for the api_codes file and the necessary api files. If the file doesn't exist, the code moves on, indicating that all of the data used will be from their respective cache files.
 try:
 	import api_codes
 	facebook_token = api_codes.facebook_token
@@ -16,9 +17,8 @@ except:
 	pass
 
 
-#open/create the cache file!
+#Here, the cache file for the faecbook data is read.
 CACHE_FNAME = "facebook_data_cache.json"
-# Put the rest of your caching setup here:
 try:
 	cache_file = open(CACHE_FNAME,'r')
 	cache_contents = cache_file.read()
@@ -27,6 +27,7 @@ try:
 except:
 	CACHE_DICTION = {}
 
+#This function calls to the Facebook API if the data is out of date. It uses the "me" object as its only parameter.
 def get_facebook_data(me):
 
 	if me in CACHE_DICTION:
@@ -36,95 +37,91 @@ def get_facebook_data(me):
 
 	else:
 		print("Fetching")
+		#Facebook's graph API is called, after which the results are returned. The results contain a list of the most recent likes a user has made, including the unix time the interaction occured.
 		graph = facebook.GraphAPI(facebook_token)
 		results = graph.get_object("me", fields = "id,name,likes", limit=100)
 		CACHE_DICTION[me] = results
 
+		#The newly-called data is stored in the cached, and now considered the most up-to-date data.
 		f = open(CACHE_FNAME, "w")
 		f.write(json.dumps(CACHE_DICTION))
 		f.close()
 
 	return results
 
+#The following set of for loops extract the data from the Facebook call.
 facebook_data = get_facebook_data("me")
 likes_data = []
 time_data = []
 date_data = []
 time_data_fixed = []
 
+#Here, a list of the likes (and their data) is extracted.
 for x in facebook_data["likes"]["data"]:
 	likes_data.append(x)
 	timestamp = x["created_time"]
 	time_data.append(timestamp)
 
+#This function seperates all of the unix timestamps into readable datestamps.
 for x in time_data:
 	y = x.split("T")
 	date_data.append(y[0])
 
+#I don't think this loop actually does anything consequential.
 for x in time_data:
 	x = x.replace("T", " ")
 	time_data_fixed.append(x)
 
-#######################
-#########WEATHER#######
-#######################
-#api_key = api_codes.weather_token
+#########################
+######## WEATHER ########
+#########################
 
-print("Fetching most recent date of Facebook Like")
+#Because the previous loops created lists of Facebook like data, index[0] of that data provides the most recent set.
+print("Fetching most recent date of Facebook Like.")
 like_date = date_data[0]
 unix_time = time_data[0]
-#print (type(like_date))
-#print (type(unix_time))
-#print(like_date)
-#print(unix_time)
-#like_date = "2017-12-12"
-#unix_time = "2017-12-12T00:13:42+0000"
 
-#US STATES
+#Open the json file containing data on state capitols. Each object contains the state name, capitol name, capitol latitude, and capitol longitude.
+#This data is moved from json into a list of tuples, containing all of the relevant data.
 state_list = []
 with open('state_data.json') as state_data:
     e = json.load(state_data)
 for x in e.items():
 	state_list.append((x[1]["name"], x[1]["capital"], x[1]["lat"], x[1]["long"]))
 
-
+#The SQLite database connection is opened. The stored date is fetched from the file. The stored date contains the date of the last time the data in the file was changed.
 conn = sqlite3.connect("state_weather_db.sqlite")
 cur = conn.cursor()
 stored_dates = cur.execute("SELECT data FROM WEATHER")
 stored_date = stored_dates.fetchone()[0]
-#print(stored_date)
-#print(like_date)
+
+#The stored date is compared against the like date. If they're equal, the cached data is up to date. If not, the API needs to be called, and new data must be fetched.
 if like_date == stored_date:
 	print("Like Date matches Storage Date. Cached data is up-to-date.")
 
-#condition - if unix-date from cache DOES NOT EQUAL unix date from facebook call, do this. That means that dates have changed
-#tups_for_print = []
+#This block is in a try because if the API doesn't go through (API down, api codes don't exist, etc.) the code can continue on using the cached data.
 try:
 	if stored_date != like_date:
 		print("Stored data is not the most recent. Fetching weather data. This may take some time!")
 		cur.execute('DELETE FROM WEATHER')
 
+		#This loop iterates through each state in the list of states, extracting the city name, capitol, and longitude for use in the DarkSky API call.
 		for item in state_list:
-
 			city_name = item[1]
 			lat = item[2]
 			lng = item[3]
 
-
+			#The DarkSky API is called using the state data. The unix_time parameter is taken from the Facebook data.
 			request_url = "https://api.darksky.net/forecast/{}/{},{},{}".format(api_key, lat, lng, unix_time)
-			#print(request_url)
 			darksky_response = requests.get(request_url)
 			darksky_data = json.loads(darksky_response.text)
-			#pprint.pprint(darksky_data)
 
+			#From the returned data, the daily high and low temperatures are stored in varibles
 			high_temp = int(darksky_data["daily"]["data"][0]["temperatureHigh"])
 			low_temp = int(darksky_data["daily"]["data"][0]["temperatureLow"])
-			#print(high_temp)
-			#print(low_temp)
-			#capital_temps.append((city_name, high_temp, low_temp, unix_time))
+
+			#All of the data collected is put into a new tuple, which is then stored in the SQL cache. This is done for all 50 states.
 			weather_tup = city_name, high_temp, low_temp, like_date
-			#weather_tup2 = "City: "city_name, "High: " high_temp, "Low": low_temp
-			#tups_for_print.append(weather_tup2)
 			cur.execute('INSERT INTO WEATHER (city, temp_hi, temp_low, data) VALUES (?,?,?,?)',weather_tup)
 
 	else:
@@ -133,43 +130,36 @@ try:
 except:
 	print("Unable to reach DarkSky API. Referring to cached data.")
 
-
 conn.commit()
-#conn.close()
 
+#########################
+#########  NEWS  ########
+#########################
 
-#cur.execute('INSERT INTO WEATHER (city, temp_hi, temp_lo, data) VALUES (?,?,?,?)',tup1)
-############################
-####### NEWS ###############
-############################
-#news_token = api_codes.news_token
-
-#url = ("https://newsapi.org/v2/top-headlines?sources=the-new-york-times&apiKey=" + news_token)
+#Like the call to the DarkSky API, this block is in a try. In case something goes wrong with the API call, the program continues with cached data.
 try:
+	#The News API is called using the like date. This fetches the most popular CNN news articles from the date of your last Facebook like.
 	url2 = ("https://newsapi.org/v2/everything?sources=cnn&from={}&to={}&sortBy=popularity&apiKey=" + news_token).format(like_date,like_date)
-	#url3 = ("https://newsapi.org/v2/everything?sources=cnn&from=2016-12-15&to=2016-12-20&sortBy=popularity&apiKey=" + news_token)
-	#print(url2)
 	response = requests.get(url2)
 	news_data = json.loads(response.text)
-	#pprint.pprint(news_data)
 
+	#From the returned data, lists of the article titles and article descriptions are created. They are then merged together, matching headlines with descriptions in a list of tuples.
 	titles_list = []
 	descriptions_list = []
 	for x in news_data["articles"]:
 		titles_list.append(x["title"])
 		descriptions_list.append(x["description"])
 	data_tups = list(zip(titles_list, descriptions_list))
-	#print(data_tups)
 
+	#This checks if the like date has changed. If it has, the data grabbed is stored in the cache and saved as the most recent data. This uses the same storage date as the weather data so that the two APIs are synchronous.
 	if stored_date != like_date:
 
+		#The headline and description from the five most popular articles are stored in a cache file. In this case, the cache is a .txt.
 		file=open("news_source.txt", "w")
-
 		for item in data_tups[:5]:
 			file.write(item[0] + "\n")
 			file.write(item[1] + "\n")
 			file.write("***" + "\n")
-
 		file.close()
 
 
@@ -183,6 +173,7 @@ except:
 ######## OUTPUT ###########
 ###########################
 
+#The following outputs the data collected by the program in a readable way.
 print("\n")
 print("*** DATA SUMMARY ***")
 print("Welcome to the data summary! Here, you can see the data that the program has collected. Let's go one API at a time. First, using a call to the Facebook API, we received the date of your most recent post like.")
@@ -195,10 +186,8 @@ print("On " + like_date + " the high and low temperatures in US state capitols w
 weather_output = []
 for row in cur.execute('SELECT * FROM WEATHER'):
 	weather_output.append(row)
-	#print(row)
 weather_output_2 = sorted(weather_output)
 for x in weather_output_2:
-	#print("City: " + x[0] + " High: " + str(x[1]) + " Low: " + str(x[2]))
 	print("{: <15} High: {: <5} Low: {: <5}".format(*x))
 print("\n")
 print("Pretty cool, right? Now, let's use the News API to dig up the top news stories from that day. Summaries of the five most popular articles from CNN can be found below.")
